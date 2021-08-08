5dde8aa314d0531363297c575e84e6ed22ac6dd0
sgasimov-dotcom <sahib.gasimov2@gmail.com>
2021-08-07 23:01:18 -0500

    sdf

---
+++ tfvars/provider.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = var.address_space
+  subnet_prefixes     = var.subnet_prefixes
+  subnet_names        = var.subnet_names
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+  depends_on = [azurerm_resource_group.example]
+}
+
+
+variable "subnet_names" {
+  type = list(any)
+}
+variable "subnet_prefixes" {
+  type = list(any)
+}
+variable "address_space" {
+}
+variable "resource_group_name" {}
+variable "location" {}
+
